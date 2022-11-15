70e1335f2732f9302104d45de37546920eb03add
linanw <linanwang@msn.com>
2022-11-16 00:55:06 +0800

    update build_install.sh to update systemd for /dev/video0 rules

--- build_install.sh
+++ build_install.sh
@@ -10,3 +10,4 @@ set -x
 sudo cp claw_machine_client.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable claw_machine_client.service
+sudo bash -c "echo -e '\nKERNEL==\"video0\", SYMLINK=\"video0\", TAG+=\"systemd\"\nKERNEL==\"video1\", SYMLINK=\"video1\", TAG+=\"systemd\"' >> /lib/udev/rules.d/99-systemd.rules"
